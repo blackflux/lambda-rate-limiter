7376fd3c449c41e15a63fedb8767d7b5b9a9ee7a
blackflux <blackflux@unfine.de>
2017-11-10 22:21:36 -0800

    chore(test): testing zero limit behaviour

--- tests/test_limiter.js
+++ tests/test_limiter.js
@@ -3,12 +3,19 @@ const limiter = require("../lib/limiter")({});
 
 describe("Testing Limiter", () => {
   it("Testing Limit Restricts", (done) => {
-    limiter.check(1, "test").then((firstCount) => {
+    limiter.check(1, "token1").then((firstCount) => {
       expect(firstCount).to.equal(1);
-      limiter.check(1, "test").catch((secondCount) => {
+      limiter.check(1, "token1").catch((secondCount) => {
         expect(secondCount).to.equal(2);
         done();
       });
+    });
+  });
+
+  it("Testing Zero Limit Fails", (done) => {
+    limiter.check(0, "token2").catch((count) => {
+      expect(count).to.equal(1);
+      done();
     });
   });
 });
