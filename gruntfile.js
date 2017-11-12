f8f438bc0897051d04e5e850cf1751338faf9500
blackflux <blackflux@unfine.de>
2017-11-11 20:03:41 -0800

    - removed update command from grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,4 @@
-const spawn = require('child_process').spawn;
-
-
 module.exports = (grunt) => {
-  // helper to run async command
-  const runCmd = (cmd, args, options) => new Promise((resolve) => {
-    grunt.log.write(`${cmd} ${args.join(" ")}\n`);
-    const ls = spawn(process.platform === 'win32' ? `${cmd}.cmd` : cmd, args, options);
-    ls.stdout.on('data', data => grunt.log.write(data));
-    ls.stderr.on('data', data => grunt.log.error(data));
-    ls.on('exit', code => grunt.log.ok(`Child process exited with code ${code}`) && resolve());
-  });
-
-  // init project config
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     eslint: {
@@ -65,14 +52,4 @@ module.exports = (grunt) => {
   grunt.loadNpmTasks('grunt-yamllint');
 
   grunt.registerTask('test', ['eslint', 'yamllint', 'mocha_istanbul:coverage']);
-
-  // update and install all npm dependencies
-  // eslint-disable-next-line func-names
-  grunt.registerTask('update', function () {
-    const cb = this.async();
-    runCmd('ncu', ['-u', '--packageFile', `package.json`])
-      .then(() => runCmd('npm', ['prune']))
-      .then(() => runCmd('npm', ['install']))
-      .then(() => cb);
-  });
 };
